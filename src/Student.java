68192883217d0fa4772cfb73d4e9d38b2159a8f2
decem122 <macos@Khan-MacBook-Air.local>
2026-06-30 16:15:00 +0700

    :sparkles:

--- src/Student.java
+++ src/Student.java
@@ -1,52 +1,14 @@
 class Student {
-    private String studentId;
-    private String name;
-    private int age;
-    private double gpa;
-
-    public double getGpa() {
-        return gpa;
-    }
-
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        if (age >= 0){
-            this.age = age;
-        }
-        else {
-            System.out.println("อายุติดลบทำไม !");
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(String studentId) {
-        this.studentId = studentId;
-    }
+    String studentId;
+    String name;
+    int age;
+    double gpa;
 
     public Student() {
         this.studentId = "0000";
         this.name = "SDDD";
         this.age = 19;
         this.gpa = 3.12;
-
     }
 
     public Student(String studentId, String name, int age, double gpa) {
@@ -57,9 +19,9 @@ class Student {
     }
 
     void printInfo() {
-        System.out.println("StudentId : " + getStudentId());
-        System.out.println("Name : " + getName());
-        System.out.println("Age : " + getAge());
-        System.out.println("Gpa : " + getGpa());
+        System.out.println("StudentId : " + studentId);
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Gpa : " + gpa);
     }
 }
